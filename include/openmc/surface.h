7bd4c550307897063b4280b409a696df3bf51a75
Patrick Shriwise <pshriwise@gmail.com>
2020-01-13 10:01:04 -0600

    Moving bounding box back into surface.

--- include/openmc/surface.h
+++ include/openmc/surface.h
@@ -10,7 +10,6 @@
 #include "hdf5.h"
 #include "pugixml.hpp"
 
-#include "openmc/bounding_box.h"
 #include "openmc/constants.h"
 #include "openmc/particle.h"
 #include "openmc/position.h"
@@ -28,6 +27,72 @@ namespace model {
   extern std::vector<std::unique_ptr<Surface>> surfaces;
   extern std::unordered_map<int, int> surface_map;
 } // namespace model
+
+//==============================================================================
+//! Coordinates for an axis-aligned cuboid bounding a geometric object.
+//==============================================================================
+
+struct BoundingBox
+{
+  double xmin = -INFTY;
+  double xmax = INFTY;
+  double ymin = -INFTY;
+  double ymax = INFTY;
+  double zmin = -INFTY;
+  double zmax = INFTY;
+
+
+  inline BoundingBox operator &(const BoundingBox& other) {
+    BoundingBox result = *this;
+    return result &= other;
+  }
+
+  inline BoundingBox operator |(const BoundingBox& other) {
+    BoundingBox result = *this;
+    return result |= other;
+  }
+
+  // intersect operator
+  inline BoundingBox& operator &=(const BoundingBox& other) {
+    xmin = std::max(xmin, other.xmin);
+    xmax = std::min(xmax, other.xmax);
+    ymin = std::max(ymin, other.ymin);
+    ymax = std::min(ymax, other.ymax);
+    zmin = std::max(zmin, other.zmin);
+    zmax = std::min(zmax, other.zmax);
+    return *this;
+  }
+
+  // union operator
+  inline BoundingBox& operator |=(const BoundingBox& other) {
+    xmin = std::min(xmin, other.xmin);
+    xmax = std::max(xmax, other.xmax);
+    ymin = std::min(ymin, other.ymin);
+    ymax = std::max(ymax, other.ymax);
+    zmin = std::min(zmin, other.zmin);
+    zmax = std::max(zmax, other.zmax);
+    return *this;
+  }
+
+  // ensure bounding box contains a point
+  inline void update(const Position& r) {
+    xmin = std::min(xmin, r.x);
+    xmax = std::max(xmax, r.x);
+    ymin = std::min(ymin, r.y);
+    ymax = std::max(ymax, r.y);
+    zmin = std::min(zmin, r.z);
+    zmax = std::max(zmax, r.z);
+  }
+
+  // check if a point is in the box
+  inline bool contains(const Position& r) const {
+    if (r.x < xmin || r.x > xmax) { return false; }
+    if (r.y < ymin || r.y > ymax) { return false; }
+    if (r.z < zmin || r.z > zmax) { return false; }
+    return true;
+  }
+
+};
 
 //==============================================================================
 //! A geometry primitive used to define regions of 3D space.
